f9a12d38ae45d762bb0fef0f456f8eacda0cece1
HaoguangHuang <734194033@qq.com>
2017-08-18 01:42:08 +0800

    add k-means segment node

--- oni2picture_ed2/tankData/MATLAB/Addpath.m
+++ oni2picture_ed2/tankData/MATLAB/Addpath.m
@@ -1,3 +1,5 @@
 % addpath(genpath('E:\Code\vs2010\oni2picture_ed2\oni2picture_ed2\tankData\MATLAB'));
 addpath(genpath('E:\Code\vs2010\oni2picture_ed2\oni2picture_ed2\tankData\MATLAB'));
 
+addpath(genpath('E:\Code\vs2010\oni2picture_ed2\oni2picture_ed2\tankData\MATLAB\node_seg'));
+
